4ffe533b61e6e68286b3c3650f3a9ef3a431a6fa
Nick Blaisdell <nick.blaisdell@gmail.com>
2019-01-28 14:49:27 -0500

    dynamic game board complete

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,14 +1,42 @@
-// codecademy minesweeper game
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+      let row = [];
+      for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+        row.push(' ');
+      }
+      board.push(row);
+  }
+  return board;
+};
+console.log(generatePlayerBoard(3,2));
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+      let row = [];
+      for (let j = 0; j < numberOfColumns; j++) {
+        row.push(null);
+      }
+      board.push(row);
+    }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] ='B';
+    numberOfBombsPlaced++;
+    // Need to add control flow to stop overwriting bomnbs
+  }
+  return board;
+};
 
-const blankline = '  |   |  '
-const guessline = '1 |   |  '
-const bombline = '  | B |  '
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
 
-console.log('This is what an empty board would look like:');
-console.log(blankline);
-console.log(blankline);
-console.log(blankline);
-console.log('This is what a board with a guess and a bomb would look like:');
-console.log(guessline);
-console.log(bombline);
-console.log(blankline);
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
